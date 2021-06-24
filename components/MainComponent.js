4c9497cf28eed4c911ac834a1edda0b7c5a3f2ae
Austin <locoman2424@gmail.com>
2021-06-24 07:11:00 -0500

    added forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Constants from 'expo-constants';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -98,6 +99,28 @@ const ContactNavigator = createStackNavigator(
       })
   }
 );
+const ReservationNavigator = createStackNavigator(
+  {
+      Reservation: { screen: Reservation }
+  },
+  {
+      defaultNavigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='tree'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
+);
 
 const HomeNavigator = createStackNavigator(
   {
@@ -161,6 +184,19 @@ const MainNavigator = createDrawerNavigator(
           drawerIcon: ({tintColor}) => (
               <Icon
                   name='list'
+                  type='font-awesome'
+                  size={24}
+                  color={tintColor}
+              />
+          )
+      }
+  },
+  Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+          drawerIcon: ({tintColor}) => (
+              <Icon
+                  name='tree'
                   type='font-awesome'
                   size={24}
                   color={tintColor}
